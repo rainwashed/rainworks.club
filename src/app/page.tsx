5146e962e71f64c8b89026ea8ff9e65d6a47af1a
rainwashed <60084007+rainwashed@users.noreply.github.com>
2025-07-28 22:35:56 -0400

    inital base site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,50 @@
 import Image from "next/image";
 
+type RainWorksMember = {
+  name: string;
+  pronouns: string;
+  github_user: string;
+}
+const members: RainWorksMember[] = [{
+  name: "Li",
+  pronouns: "he/him",
+  github_user: "rainwashed"
+}, {
+  name: "Joshua",
+  pronouns: "he/him",
+  github_user: "JJCgits"
+}, {
+  name: "xaft",
+  pronouns: "he/him",
+  github_user: "xt40"
+}]
+
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <>
+      <div className="flex flex-col items-center justify-center w-screen min-h-screen space-y-2">
+        <h1 className="text-5xl">rainworks.club</h1>
+        <p>
+          We are a self-governed team of programmers building the solutions for tomorrow&apos;s problems.
+        </p>
+        <span className="px-4 py-2 rounded-md border-foreground/20 border-1">
+          <p>Check out our <a href="https://github.com/rainworks-software" target="_blank" className="underline">GitHub</a> to see some projects.</p>
+        </span>
+      </div>
+      <div className="flex items-center justify-center w-screen">
+        <div className="flex flex-col p-4 space-y-2 border rounded-lg border-foreground/20">
+          <span>Our members:</span>
+          <span className="inline-flex space-x-4">
+            {members.map((member, i) => <div key={i} className="inline-flex flex-col items-center">
+              <a href={`https://github.com/${member.github_user}`} target="_blank">
+                <Image src={`https://github.com/${member.github_user}.png`} alt={member.github_user} width={64} height={64} className={`rounded-full ${i === 0 ? 'border-2 p-0.5 border-yellow-400 outline-offset-8' : ''}`} />
+              </a>
+              <span>{member.name} • {member.pronouns}</span>
+            </div>)}
+          </span>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+      <div className="h-20" />
+    </>
   );
 }
